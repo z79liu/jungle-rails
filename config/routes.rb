c903042f80497233e735b67bf7da79abee5e692c
Zeyu <haozai_03@hotmail.com>
2019-04-18 19:18:27 +0000

    added login/register page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # resources :users, only: [:new, :create]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'  
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
